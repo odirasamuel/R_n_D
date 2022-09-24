777bdb8e85b07e9bfe77890c0268c93bfb710afb
Odira Samuel <odira.samuel.ifeanyi@gmail.com>
2022-09-24 15:03:02 -0500

    INFRA-4

--- vpc/providers.tf
+++ vpc/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  alias  = "dev"
-  region = "us-east-1"
+  alias   = "dev"
+  region  = "us-east-1"
   profile = "odira"
 
   default_tags {
@@ -11,8 +11,8 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "prod"
-  region = "us-west-1"
+  alias   = "prod"
+  region  = "us-west-1"
   profile = "odira"
 
   default_tags {
